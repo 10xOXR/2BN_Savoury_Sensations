dfa348a0ca2ed8821c5e9e551c1e953052fc312d
10xOXR <10xoxr@gmail.com>
2019-05-04 21:04:36 +0100

    Removed unnecessary import from app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import random
 import math
-import js2py
 from flask import Flask, render_template, redirect, request, url_for, flash, Markup, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
